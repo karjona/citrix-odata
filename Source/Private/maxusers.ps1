7f8bd8f6dd0e9e208f10798f7ecf5a9099220c5f
Kilian Arjona <karjona@gmail.com>
2019-08-13 16:31:19 +0200

    Create Get-CitrixMonitorServiceData file and help

--- Source/Private/maxusers.ps1
+++ Source/Private/maxusers.ps1
@@ -1,48 +1,50 @@
-$username = 'EURO\ae028793'
-$password = ConvertTo-SecureString 'XxXXx' -AsPlainText -Force
-$ddcs = @('ffxgbsphxdc01.euro.iberdrola.local', 'ffxgbsphxdc03.euro.iberdrola.local')
-$startdate = '2019-08-07'
-$enddate = '2019-08-07'
-
-
-
-
-
-$cred = New-Object System.Management.Automation.PSCredential($username, $password)
-$ddcObj = @()
-
-foreach ($ddc in $ddcs) {
-    $dgroups = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/DesktopGroups?`$format=json" -Credential $cred
+function maxusers {
+    $username = 'xx'
+    $password = ConvertTo-SecureString 'XxXXx' -AsPlainText -Force
+    $ddcs = @('xx', 'xx')
+    $startdate = '2019-08-07'
+    $enddate = '2019-08-07'
     
-    $dgroupObj = @()
-    foreach ($dgroup in $dgroups.value) {
-        $activity = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/SessionActivitySummaries?`$format=json&`$filter=(DesktopGroupId eq guid'$($dgroup.Id)') and (SummaryDate gt DateTime'$startdate`T00:00:00') and (SummaryDate lt DateTime'$enddate`T23:59:59') and (Granularity eq 1440)" -Credential $cred
-        $machines = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Machines?`$format=json&`$filter=(DesktopGroupId eq guid'$($dgroup.Id)')" -Credential $cred
+    
+    
+    
+    
+    $cred = New-Object System.Management.Automation.PSCredential($username, $password)
+    $ddcObj = @()
+    
+    foreach ($ddc in $ddcs) {
+        $dgroups = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/DesktopGroups?`$format=json" -Credential $cred
         
-        $concurrentUsers = $activity.value.ConcurrentSessionCount | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
-        if ($null -eq $concurrentUsers) {
-            $concurrentUsers = 0
+        $dgroupObj = @()
+        foreach ($dgroup in $dgroups.value) {
+            $activity = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/SessionActivitySummaries?`$format=json&`$filter=(DesktopGroupId eq guid'$($dgroup.Id)') and (SummaryDate gt DateTime'$startdate`T00:00:00') and (SummaryDate lt DateTime'$enddate`T23:59:59') and (Granularity eq 1440)" -Credential $cred
+            $machines = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Machines?`$format=json&`$filter=(DesktopGroupId eq guid'$($dgroup.Id)')" -Credential $cred
+            
+            $concurrentUsers = $activity.value.ConcurrentSessionCount | Measure-Object -Maximum | Select-Object -ExpandProperty Maximum
+            if ($null -eq $concurrentUsers) {
+                $concurrentUsers = 0
+            }
+            
+            $dgroupObj += [PSCustomObject]@{
+                Id = $dgroup.Id
+                Name = $dgroup.Name
+                ConcurrentUsers = $concurrentUsers
+                VMNumber = $($machines.value | Where-Object -FilterScript {$_.HostedMachineName -NE $null -and $_.LifecycleState -eq 0 -and $_.IsPendingUpdate -ne $true} | Measure-Object | Select-Object -ExpandProperty Count)
+            }
         }
         
-        $dgroupObj += [PSCustomObject]@{
-            Id = $dgroup.Id
-            Name = $dgroup.Name
-            ConcurrentUsers = $concurrentUsers
-            VMNumber = $($machines.value | Where-Object -FilterScript {$_.HostedMachineName -NE $null -and $_.LifecycleState -eq 0 -and $_.IsPendingUpdate -ne $true} | Measure-Object | Select-Object -ExpandProperty Count)
+        $ddcObj += [PSCustomObject]@{
+            Name = $ddc
+            DeliveryGroups = $dgroupObj
         }
     }
     
-    $ddcObj += [PSCustomObject]@{
-        Name = $ddc
-        DeliveryGroups = $dgroupObj
+    $capacityObj = [PSCustomObject]@{
+        CreationDate = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"
+        StartDate = "$startdate`T00:00:00"
+        EndDate = "$enddate`T23:59:59"
+        CitrixHosts = $ddcObj
     }
+    
+    ConvertTo-Json -InputObject $capacityObj -Depth 3
 }
-
-$capacityObj = [PSCustomObject]@{
-    CreationDate = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"
-    StartDate = "$startdate`T00:00:00"
-    EndDate = "$enddate`T23:59:59"
-    CitrixHosts = $ddcObj
-}
-
-ConvertTo-Json -InputObject $capacityObj -Depth 3
--- Source/Private/sessions.ps1
+++ Source/Private/sessions.ps1
@@ -1,80 +1,82 @@
-$username = 'SD\administrator'
-$password = ConvertTo-SecureString 'XXX' -AsPlainText -Force
-$ddcs = @('sdcontroller61.sd.local')
-$date = '2019-08-11'
-
-
-
-
-
-function CountSessions {
-    param([PSCustomObject]$s, [PSCustomObject]$m)
-    foreach ($f in $s.value) {
-        $name = $m | Where-Object -Property Id -EQ $f.MachineId | Select-Object -ExpandProperty DesktopGroupName
-        if ($null -eq $name) {
-            $name = "No delivery group assigned"
-        }
-        $dgroupsessions[$name] += 1
-    }
-}
-
-$cred = New-Object System.Management.Automation.PSCredential($username, $password)
-$ddcObj = @()
-$m = @()
-$dgroupsessions = @{}
-
-foreach ($ddc in $ddcs) {
-    $dgroups = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/DesktopGroups?`$format=json&`$select=Id,Name" -Credential $cred
-    $machines = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Machines?`$format=json&`$select=Id,DesktopGroupId&`$filter=(DesktopGroupId ne null) and (Id ne null)" -Credential $cred
-    foreach ($machine in $machines.value) {
-        $m += [PSCustomObject]@{
-            Id = $machine.Id
-            DesktopGroupName = $dgroups.value | Where-Object -Property Id -EQ $machine.DesktopGroupId | Select-Object -ExpandProperty Name
+function sessions {
+    $username = 'xx'
+    $password = ConvertTo-SecureString 'XXX' -AsPlainText -Force
+    $ddcs = @('xx')
+    $date = '2019-08-11'
+    
+    
+    
+    
+    
+    function CountSessions {
+        param([PSCustomObject]$s, [PSCustomObject]$m)
+        foreach ($f in $s.value) {
+            $name = $m | Where-Object -Property Id -EQ $f.MachineId | Select-Object -ExpandProperty DesktopGroupName
+            if ($null -eq $name) {
+                $name = "No delivery group assigned"
+            }
+            $dgroupsessions[$name] += 1
         }
     }
     
-    $sessions = 0
-    # Sessions that started earlier than the selected date, but ended on the selected date
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and ((EndDate gt DateTime'$date`T00:00:00') and (EndDate lt DateTime'$date`T23:59:59'))" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
+    $cred = New-Object System.Management.Automation.PSCredential($username, $password)
+    $ddcObj = @()
+    $m = @()
+    $dgroupsessions = @{}
     
-    # Sessions that started on the selected date and are still active
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and (EndDate eq null)" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
-    
-    # Sessions that started earlier than the selected date and ended later
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and (EndDate gt DateTime'$date`T23:59:59')" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
-    
-    # Sessions that started and ended on the selected date, typically the larger amount
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate gt DateTime'$date`T00:00:00') and (EndDate lt DateTime'$date`T23:59:59')" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
-    
-    # Sessions that started on the selected date and are still active
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=((StartDate gt DateTime'$date`T00:00:00') and (StartDate lt DateTime'$date`T23:59:59')) and (EndDate eq null)" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
-    
-    # Sessions that started on the selected date and ended later
-    $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=((StartDate gt DateTime'$date`T00:00:00') and (StartDate lt DateTime'$date`T23:59:59')) and (EndDate gt DateTime'$date`T23:59:59')" -Credential $cred
-    $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
-    CountSessions $s $m
-    
-    $ddcObj += [PSCustomObject]@{
-        Name = $ddc
-        Sessions = $sessions
+    foreach ($ddc in $ddcs) {
+        $dgroups = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/DesktopGroups?`$format=json&`$select=Id,Name" -Credential $cred
+        $machines = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Machines?`$format=json&`$select=Id,DesktopGroupId&`$filter=(DesktopGroupId ne null) and (Id ne null)" -Credential $cred
+        foreach ($machine in $machines.value) {
+            $m += [PSCustomObject]@{
+                Id = $machine.Id
+                DesktopGroupName = $dgroups.value | Where-Object -Property Id -EQ $machine.DesktopGroupId | Select-Object -ExpandProperty Name
+            }
+        }
+        
+        $sessions = 0
+        # Sessions that started earlier than the selected date, but ended on the selected date
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and ((EndDate gt DateTime'$date`T00:00:00') and (EndDate lt DateTime'$date`T23:59:59'))" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        # Sessions that started on the selected date and are still active
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and (EndDate eq null)" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        # Sessions that started earlier than the selected date and ended later
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate lt DateTime'$date`T00:00:00') and (EndDate gt DateTime'$date`T23:59:59')" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        # Sessions that started and ended on the selected date, typically the larger amount
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=(StartDate gt DateTime'$date`T00:00:00') and (EndDate lt DateTime'$date`T23:59:59')" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        # Sessions that started on the selected date and are still active
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=((StartDate gt DateTime'$date`T00:00:00') and (StartDate lt DateTime'$date`T23:59:59')) and (EndDate eq null)" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        # Sessions that started on the selected date and ended later
+        $s = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/Sessions?`$format=json&`$filter=((StartDate gt DateTime'$date`T00:00:00') and (StartDate lt DateTime'$date`T23:59:59')) and (EndDate gt DateTime'$date`T23:59:59')" -Credential $cred
+        $sessions += $s.value | Measure-Object -Maximum | Select-Object -ExpandProperty Count
+        CountSessions $s $m
+        
+        $ddcObj += [PSCustomObject]@{
+            Name = $ddc
+            Sessions = $sessions
+        }
     }
+    
+    $capacityObj = [PSCustomObject]@{
+        CreationDate = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"
+        ReportDate = $date
+        CitrixHosts = $ddcObj
+    }
+    
+    ConvertTo-Json -InputObject $capacityObj -Depth 2
+    $dgroupsessions
 }
-
-$capacityObj = [PSCustomObject]@{
-    CreationDate = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"
-    ReportDate = $date
-    CitrixHosts = $ddcObj
-}
-
-ConvertTo-Json -InputObject $capacityObj -Depth 2
-$dgroupsessions
