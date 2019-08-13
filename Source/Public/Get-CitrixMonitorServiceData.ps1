965ac23052e16158814a4a476afdb37e532a124b
Kilian Arjona <karjona@gmail.com>
2019-08-13 19:31:48 +0200

    Handle OData Service connection logic

--- Source/Public/Get-CitrixMonitorServiceData.ps1
+++ Source/Public/Get-CitrixMonitorServiceData.ps1
@@ -57,25 +57,79 @@ function Get-CitrixMonitorServiceData {
     [OutputType('PSCustomObject')]
 
     param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, Position=0, HelpMessage='Enter one or more Delivery' +
+        ' Controllers separated by commas.')]
+        [Alias('ComputerName')]
+        [String[]]
+        $DeliveryControllers,
+
         [Parameter()]
-        [String[]]$DeliveryControllers,
+        [PSCredential]
+        $Credential,
+
         [Parameter()]
-        [PSCredential]$Credential,
+        [DateTime]
+        $StartDate = "$(Get-Date (Get-Date).AddDays(-1) -Format 'yyyy-MM-ddT00:00:00')",
+
         [Parameter()]
-        [DateTime]$StartDate,
-        [Parameter()]
-        [DateTime]$EndDate
+        [DateTime]
+        $EndDate = "$(Get-Date (Get-Date).AddDays(-1) -Format 'yyyy-MM-ddT23:59:59')"
     )
 
-    Begin {
-
+    begin {
+        # Test if Delivery Controllers are reachable and credentials are valid
+        # Delivery Controllers that do not respond or that have invalid credentials will be removed from the list
+        # If no Delivery Controllers are left after the validation, the cmdlet execution is halted
+        foreach ($ddc in $DeliveryControllers) {
+            try {
+                if (!$Credential) {
+                    $TestConnectionRequest = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/" `
+                    -UseDefaultCredentials
+                } else {
+                    $TestConnectionRequest = Invoke-RestMethod -Uri "http://$ddc/Citrix/Monitor/OData/v3/Data/" `
+                    -Credential $Credential
+                }
+            } catch {
+                $ConnectionError = $_
+                # Handle 401 (invalid credentials) error
+                if ($ConnectionError.Exception.Response.StatusCode) {
+                    if ($ConnectionError.Exception.Response.StatusCode.ToString() -eq 'Unauthorized') {
+                        if (!$Credential) {
+                            Write-Error ("The current user does not have at least read-only administrator " +
+                            "permissions on $ddc.")
+                        } else {
+                            Write-Error ("The supplied credentials do not have at least read-only administrator " +
+                            "permissions on $ddc.")
+                        }
+                    # There's a web server on that address, but responded with an error
+                    } else {
+                        Write-Error ("The server on $ddc responded with an error: " +
+                        "$($ConnectionError.Exception.Message)")
+                    }
+                } else {
+                    # Handle DNS resolution errors
+                    if ($ConnectionError.Exception.Status.ToString() -eq 'NameResolutionFailure') {
+                        Write-Error "Could not find host $ddc."
+                    # Handle all other errors
+                    } else {
+                        Write-Error ("An error occurred while trying to connect to $ddc. Check network " +
+                        "connectivity and that the specified host is a Citrix Delivery Controller.`r`n" +
+                        "$($ConnectionError.Exception.Message)")
+                    }
+                }
+                # Remove the failed DDC from the DDCs list
+                $DeliveryControllers = $DeliveryControllers | Where-Object -FilterScript {$_ -ne $ddc}
+            } finally {
+                if (!$DeliveryControllers) {
+                    throw "Could not connect to any of the specified Delivery Controllers."
+                }
+            }
+        }
     }
 
-    Process {
-        Write-Host 'This is my test.'
-    }
-
-    End {
-
+    process {
+        Write-Host $DeliveryControllers
+        Write-Host $StartDate
+        Write-Host $EndDate
     }
 }
