41236338a53b290611f4d3ca76199e62b6371e8a
Kilian Arjona <karjona@gmail.com>
2019-10-10 19:22:50 +0200

    Updated Get private cmdlets documentation

--- Source/Private/Get-CitrixMachinesForDG.ps1
+++ Source/Private/Get-CitrixMachinesForDG.ps1
@@ -2,19 +2,26 @@ function Get-CitrixMachinesForDG {
     
     <#
     .SYNOPSIS
+    Returns the number of managed machines in the specified Citrix Virtual Apps and Desktops Delivery Group.
     
     .DESCRIPTION
+    The Get-CitrixMachinesForDG cmdlet returns an integer with the number of managed machines in a specified
+    Delivery Group. This result is directly retrieved from the Citrix Monitor Service OData API.
     
     .LINK
     https://github.com/karjona/citrix-odata
     
     .PARAMETER MachinesObject
+    Specifies an object that contains the results from a query to the Machines endpoint of the Citrix OData Monitor
+    Service API. This object can be easily generated with the Get-CitrixMachines cmdlet.
     
     .PARAMETER DeliveryGroupId
+    Specifies a single Delivery Group ID to collect data from.
     
     .COMPONENT
     citrix-odata
     #>
+    
     
     [CmdletBinding()]
     [OutputType('int')]
@@ -30,8 +37,8 @@ function Get-CitrixMachinesForDG {
     )
     
     process {
-        $MachinesForDeliveryGroup = $MachinesObject.value | `
-        Where-Object -FilterScript {$_.DesktopGroupId -eq $DeliveryGroupId} | `
+        $MachinesForDeliveryGroup = $MachinesObject.value |
+        Where-Object -FilterScript {$_.DesktopGroupId -eq $DeliveryGroupId} |
         Select-Object -Property HostedMachineId -Unique | Measure-Object | Select-Object -ExpandProperty Count
         if ($null -eq $MachinesForDeliveryGroup) {
             $MachinesForDeliveryGroup = 0
--- Source/Private/Get-CitrixMaximumSessionsForDG.ps1
+++ Source/Private/Get-CitrixMaximumSessionsForDG.ps1
@@ -2,19 +2,30 @@ function Get-CitrixMaximumSessionsForDG {
     
     <#
     .SYNOPSIS
+    Returns the maximum number of concurrent sessions in the specified Citrix Virtual Apps and Desktops Delivery
+    Group.
     
     .DESCRIPTION
+    The Get-CitrixMaximumSessionsForDG cmdlet returns an integer with themaximum number of concurrent sessions in a
+    specified Delivery Group. The concurrent session information is retrieve directly from the Citrix Monitor
+    Service OData API and then sorted with this cmdlet to return the maximum observed value among the retrieved
+    data.
     
     .LINK
     https://github.com/karjona/citrix-odata
     
     .PARAMETER SessionsObject
+    Specifies an object that contains the results from a query to the SessionActivitySummaries endpoint of the
+    Citrix OData Monitor Service API. This object can be easily generated with the Get-CitrixSessionActivity
+    cmdlet.
     
     .PARAMETER DeliveryGroupId
+    Specifies a single Delivery Group ID to collect data from.
     
     .COMPONENT
     citrix-odata
     #>
+    
     
     [CmdletBinding()]
     [OutputType('int')]
@@ -30,9 +41,9 @@ function Get-CitrixMaximumSessionsForDG {
     )
     
     process {
-        $MaxSessionsForDeliveryGroup = $SessionsObject.value | Group-Object -Property DesktopGroupId | `
-        Where-Object -FilterScript {$_.Name -eq $DeliveryGroupId } | Select-Object -ExpandProperty Group | `
-        Sort-Object -Property ConcurrentSessionCount -Descending | `
+        $MaxSessionsForDeliveryGroup = $SessionsObject.value | Group-Object -Property DesktopGroupId |
+        Where-Object -FilterScript {$_.Name -eq $DeliveryGroupId } | Select-Object -ExpandProperty Group |
+        Sort-Object -Property ConcurrentSessionCount -Descending |
         Select-Object -ExpandProperty ConcurrentSessionCount -First 1
         if ($null -eq $MaxSessionsForDeliveryGroup) {
             $MaxSessionsForDeliveryGroup = 0
